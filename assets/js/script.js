620bbdf95a7e24bb93f08e4d8d765c4e9a506bf3
Lydia Young <lydsjoyy@gmail.com>
2023-05-29 15:27:10 +0000

    Add event listener for buttons

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector(".button");
+const scores = document.querySelector(".scores");
+let player; 
+let computer; 
+let score; 
+
+button.forEach(button => button.addEventListener("click,"() => {
+
+    player = button.textContent;
+    computerTurn();
+
+}));
+
+function computerTurn(){
+
+}
