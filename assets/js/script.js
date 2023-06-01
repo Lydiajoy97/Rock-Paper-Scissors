285f2340ace1843ff65a5da226bfeb29b0107b61
Lydia Young <lydsjoyy@gmail.com>
2023-06-01 15:16:01 +0000

    Add inittal functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,29 @@
 var rock = 0;
 var paper = 1;
 var scissors = 2;
+
+var userChoice; 
+
+function handleRock(e) {
+  userChoice = rock;
+}
+
+function handlePaper(e) {
+  userChoice = paper;
+}
+
+function handleScissors(e) {
+  userChoice = scissors
+}
+
+function handlePlay(e) {
+  
+}
+
+function generateCPU() {
+
+}
+
+function calculateWinner(user, CPU) {
+  
+}
