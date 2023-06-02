a7016e8d7a711df742cbe8284087548edc11a931
Lydia Young <lydsjoyy@gmail.com>
2023-06-02 13:23:21 +0000

    Add if else statments to calculate winner function

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ function handleScissors(e) {
 }
 
 function handlePlay(e) {
-  console.log("User choice is: " + userChoice);
+  console.log("User choice is: rock" + userChoice);
   let CPUChoice = generateCPU();
 }
 
@@ -27,5 +27,26 @@ function generateCPU() {
 }
 
 function calculateWinner(user, CPU) {
-
+// if statements here
+if ( userChoice === CPUChoice) {
+  return "It's a Draw!"
+}
+else if (userChoice === 0 && CPUChoice === 1) {
+ return "You lose! The computer chose ${CPUChoice} "
+}
+else if (userChoice === 1 && CPUChoice === 0 ) {
+  "Wahoo! you win! The Computer chose ${CPUChoice} and paper covers rock."
+}
+else if (userChoice === 1 && CPUChoice === 2) {
+return "You lose! The computer chose ${CPUChoice} "
+}
+else if (userChoice === 2 && CPUChoice === 1) {
+ return "Hooray! You win! The computer chose ${CPUChoice}."
+}
+else if (userChoice === 0 && CPUChoice === 2) {
+  return "Nice! The Computer chose ${CPUChoice} and rock crushes scissors!"
+}
+else if (userChoice === 2 && CPUChoice === 0) {
+  return "Too bad... The Computer chose ${CPUChoice} and rock crushes scissors."
+}
 }
