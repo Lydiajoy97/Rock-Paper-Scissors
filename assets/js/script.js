3c7a10ab542eec543e25984403edc92823e2a567
Lydia Young <lydsjoyy@gmail.com>
2023-06-03 13:00:27 +0000

    Add functions to click event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,56 @@
 const CPUChoiceDisplay = document.getElementById('cpu-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('results');
-const possibleChoices = document.querySelector('button');
+const possibleChoices = document.querySelectorAll('button');
 let userChoice
 let CPUChoice
+let score
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
 userChoice = e.target.id
 userChoiceDisplay.innerHTML = userChoice
-CPUChoiceDisplay.innerHTML = CPUChoice
+generateCPU();
+calculateWinner();
 } ))
 
 function generateCPU(e) {
  const randomNumber = Math.floor(Math.random() * 3) +1;
  console.log(randomNumber);
+ if (randomNumber === 1){
+  CPUChoice = "rock"
+ }
+ if (randomNumber === 2){
+  CPUChoice = "paper"
+ }
+ if (randomNumber === 3){
+  CPUChoice = "scissors"
+ }
+ CPUChoiceDisplay.innerHTML = CPUChoice
 }
 
 function calculateWinner() {
 if ( userChoice === CPUChoice) {
-  h4.innerText = "Its a draw!"
+  result = "Draw!"
 }
 else if (userChoice === "rock" && CPUChoice === "paper" || userChoice === "paper" && CPUChoice === "scissors" ||
  userChoice === "scissors" && CPUChoice === "rock") {
- h4.innerText = "Too bad!"
- return incrementCPUResult();
+ result = "Too bad... You lost!"
 }
 else if (userChoice === "paper" && CPUChoice === "rock" || userChoice === "scissors" && CPUChoice === "paper" || 
 userChoice === "rock" && CPUChoice === "scissors") {
- h4.innerText = "You got it!"
- return incrementScore();
+ result = "You won! Wahoo!"
 }
+resultDisplay.innerHTML = result
 }
 
 // This adds a score of 1 to the user or computer depending on who wins. Written using the Love Maths Tutorial.
 
-function incrementScore() {
-  let oldScore = parseInt(getElementsById("userChoice").innerText);
-  document.getElementsById("userChoice").innerText = ++oldScore;
+function AddUserScore() {
+  let oldScore = parseInt(getElementsById("#user-score").innerText);
+  document.getElementsById("#user-score").innerText = oldScore + 1;
 }
 
-function incrementCPUResult() {
-  let CPUOldScore = parseInt(getElementsById("CPUChoice").innerText);
-  document.getElementsById("CPUChoice").innerText = ++CPUOldScore;
+function AddCPUResults() {
+  let CPUOldScore = parseInt(getElementsById("#cpu-score").innerText);
+  document.getElementsById("#cpu-score").innerText = CPUOldScore +1;
 }
