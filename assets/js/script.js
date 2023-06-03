d8325b6557370616a8f0d08be84c077cb70da904
Lydia Young <lydsjoyy@gmail.com>
2023-06-03 20:27:08 +0000

    Edit css button styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 const CPUChoiceDisplay = document.getElementById('cpu-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('results');
-const possibleChoices = document.querySelectorAll('button');
+const possibleChoices = document.querySelectorAll("button");
 let userChoice
 let CPUChoice
 let score = 0 
