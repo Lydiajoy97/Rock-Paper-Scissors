9a1479ac580270d7daa769599c0dda8bcddf2885
Lydia Young <lydsjoyy@gmail.com>
2023-06-03 10:21:27 +0000

    Edit inital functions and tidy up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,32 @@
-// Play game
+// Game variables
 var rock = 0;
 var paper = 1;
 var scissors = 2;
 
-var userChoice; 
-
 function handleRock(e) {
   userChoice = rock;
+  return userChoice;
 }
 
 function handlePaper(e) {
   userChoice = paper;
+  return userChoice;
 }
 
 function handleScissors(e) {
   userChoice = scissors
+  return userChoice;
 }
 
-function handlePlay(e) {
-  let userChoice = element.addEventListener("click");
-  let CPUChoice = Math.random() * (2-0) +1;
-  let result = calculateWinner();
+Button.addEventListener("click", compareCPUVsUser);
+
+function randomChoice(e) {
+  var randomChoice = Math.floor(Math.random() * (0-2) +1);
 }
 
-// This adds a score of 1 to the user or computer depending on who wins. Written using the Love Maths Tutorial.
-
-function incrementScore() {
-  let oldScore = parseInt(getElementsById("userChoice").innerText);
-  document.getElementsById("userChoice").innerText = ++oldScore;
-}
-
-function incrementCPUResult() {
-  let CPUOldScore = parseInt(getElementsById("CPUChoice").innerText);
-  document.getElementsById("CPUChoice").innerText = ++CPUOldScore;
+function getCPUChoice() {
+ var CPUChoice = randomChoice();
+ return CPUChoice;
 }
 
 function calculateWinner(user, CPU) {
@@ -58,4 +52,16 @@ else if (userChoice === 0 && CPUChoice === 2) {
 else if (userChoice === 2 && CPUChoice === 0) {
   result = CPUChoice;
 }
+}
+
+// This adds a score of 1 to the user or computer depending on who wins. Written using the Love Maths Tutorial.
+
+function incrementScore() {
+  let oldScore = parseInt(getElementsById("userChoice").innerText);
+  document.getElementsById("userChoice").innerText = ++oldScore;
+}
+
+function incrementCPUResult() {
+  let CPUOldScore = parseInt(getElementsById("CPUChoice").innerText);
+  document.getElementsById("CPUChoice").innerText = ++CPUOldScore;
 }
